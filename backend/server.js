a573d1c553f205060d4905042237f7bfd31bd187
Saltypeesh <tungthanh254@gmail.com>
2025-03-31 02:37:54 +0700

    Database setup - Remove .env file and update server configuration to use port 5000. - Integrate database connection and authentication routes.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import { connectDB } from "./db/connectDB.js";
+
+import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 
 const app = express();
@@ -9,8 +13,11 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-const PORT = process.env.PORT || 3000;
+app.use('/api/auth', authRouter);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
